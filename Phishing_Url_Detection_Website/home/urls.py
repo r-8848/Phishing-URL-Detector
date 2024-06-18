f846eda00c9be9297f4a6ad179833c1425c87133
r-8848 <146222309+r-8848@users.noreply.github.com>
2024-06-18 21:09:32 +0530

    urls.py

--- Phishing_Url_Detection_Website/home/urls.py
+++ Phishing_Url_Detection_Website/home/urls.py
@@ -1,7 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
-from . import views
+from home import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('detect-phishing/', views.detect_phishing, name='detect_phishing'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
