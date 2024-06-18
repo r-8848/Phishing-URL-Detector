8439dafb5fc8c4051b5036420510bffa96913165
r-8848 <146222309+r-8848@users.noreply.github.com>
2024-06-18 21:08:56 +0530

    settings.py

--- Phishing_Url_Detection_Website/phishing_detection/settings.py
+++ Phishing_Url_Detection_Website/phishing_detection/settings.py
@@ -122,3 +122,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# settings.py
+# The following line is typically used in development only
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
